63a47a57a7869368cf3445296cec9709483d10b4
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-25 12:01:50 -0500

    add basic markup and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
+import { faAngleRight, faClose, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { RouterLink } from 'vue-router';
+
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
+    <header class="text-xl">
+      <button class="p-2" popovertarget="main-nav">La Lista</button>
 
-    <header>
+      <nav popover id="main-nav" class="open:flex flex-col min-w-screen bg-sky-950 text-ash backdrop:backdrop-blur-sm">
+
+        <button class="p-2 flex justify-between items-center border-b border-ash/20" popovertarget="main-nav">
+          La Lista
+          <FontAwesomeIcon :icon="faClose" />
+        </button>
+
+        <RouterLink to="/recipes" class="p-2 flex justify-between items-center border-b border-ash/20">
+          Recipes
+          <FontAwesomeIcon :icon="faAngleRight" />
+        </RouterLink>
+
+        <RouterLink :to="{
+          name: 'about'
+        }" class="p-2 flex justify-between items-center border-b border-ash/20">
+
+          About
+          <FontAwesomeIcon :icon="faAngleRight" />
+        </RouterLink>
+      </nav>
+
       <button>
-        La Lista
-      </button>
-      <button>
-        <FontAwesomeIcon :icon="fa" />
+        <FontAwesomeIcon :icon="faPlus" />
       </button>
     </header>
 
