6f788af9d96e044d0089bc78049ae7fe537d653f
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-25 11:46:30 -0500

    refactor code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <template>
-  <main>
-    <h1 class="text-xl">this is home</h1>
-  </main>
+  <div class="flex flex-col min-h-screen">
+
+    <header>
+      <button>
+        La Lista
+      </button>
+      <button>
+        <FontAwesomeIcon :icon="fa" />
+      </button>
+    </header>
+
+    <main class="grow">
+      <h1 class="text-xl">this is home</h1>
+    </main>
+
+    <footer>here the footer</footer>
+  </div>
 </template>
