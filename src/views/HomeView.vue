b3559996cc82bfcf662206319187428d0533ea60
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-25 12:20:25 -0500

    refactor code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen gap-2">
     <header class="text-xl flex justify-between items-center border-b border-ash/20">
 
       <button class="p-2 flex items-center gap-2" popovertarget="main-nav">
@@ -22,8 +22,8 @@ import { RouterLink } from 'vue-router';
 
     </header>
 
-    <main class="grow">
-      <h1 class="text-xl">this is home</h1>
+    <main class="grow px-2">
+
     </main>
 
     <footer class="py-2 text-center text-sm">
