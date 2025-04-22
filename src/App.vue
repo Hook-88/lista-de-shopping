a23f6b474c4c1ce7d8a4df01fa40f9d56d66639d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-04-22 16:42:40 -0500

    add pag-footer

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen">
+    <RouterView />
+    <footer class="mt-auto py-1 text-center">
+      <small>&copy; 2025 - Hook-88 b.v.</small>
+    </footer>
+  </div>
 </template>
