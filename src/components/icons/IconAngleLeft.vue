1e18917a8de81105975894554451493a23a924e4
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-04-24 08:47:00 -0500

    improved styles

---
+++ src/components/icons/IconAngleLeft.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons/faAngleLeft';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+</script>
+
+<template>
+  <FontAwesomeIcon :icon="faAngleLeft" />
+</template>
--- src/views/AddShoppingItemView.vue
+++ src/views/AddShoppingItemView.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import IconAngleLeft from '@/components/icons/IconAngleLeft.vue';
 import IconLink from '@/components/links/IconLink.vue';
 import PageHeader from '@/components/page-header/PageHeader.vue';
 
 </script>
 
 <template>
-  <PageHeader>
-    <IconLink :to="{ name: 'home' }">Back</IconLink>
-    <h1>Add new item</h1>
+  <PageHeader class="grid grid-cols-4">
+
+    <IconLink :to="{ name: 'home' }" class="text-base flex items-center gap-1 text-blue-600">
+      <IconAngleLeft />
+      Back
+    </IconLink>
+
+    <h1 class="grow text-center col-span-2 py-1">Add new item</h1>
+
   </PageHeader>
   <main class="p-2">
 
